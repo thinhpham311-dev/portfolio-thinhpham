5003190e60c0b2f917fbbcbac7c80c1e2df022e2
thinhpham <phamthinh30522@gmail.com>
2023-12-02 07:51:29 -0800

    update code

--- src/components/Sidebar/styles.tsx
+++ src/components/Sidebar/styles.tsx
@@ -8,7 +8,7 @@ const SidebarSectionWrapper = styled.section(() => [
 ])
 
 const SidebarAvatar = styled.div(() => [
-    tw` max-w-full  overflow-hidden mx-auto h-[300px] flex flex-col justify-between py-2 my-5`,
+    tw` max-w-full  overflow-hidden mx-auto h-[300px] flex flex-col justify-between my-5`,
     css`
         img{
             ${tw`h-[200px] w-[200px] object-cover rounded-full border-8 border-white shadow-base mx-auto mb-[10px]`}
@@ -29,12 +29,12 @@ const SidebarBody = styled.div(() => [
 ])
 
 const SidebarMenu = styled.div(() => [
-    tw`text-center overflow-y-auto overflow-x-hidden h-[calc(100%-340px)]`,
+    tw`text-center overflow-y-auto overflow-x-hidden h-[calc(100%-332px)]`,
     css`
         ul{
             ${tw`grid gap-y-3 py-3`}
             a{
-                ${tw`w-full h-[auto] p-[20px] block border-2 border-black rounded-full shadow-base`}
+                ${tw`w-full h-[auto] p-[15px] block border-2 border-black rounded-full shadow-base`}
             }
         }
     `
